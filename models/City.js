c31d3eafa8a3522157322ba0bc7357fff0145b8f
Alejandro Vargas <alejovastillo@gmail.com>
2024-10-21 18:36:36 -0500

    Script cities

---
+++ models/City.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+let collection = "Cities";
+
+const citySchema = new Schema(
+  {
+    name: { type: String, required: true },
+    country: { type: String, required: true },
+    population: { type: Number, required: true },
+    area: { type: Number, required: true },
+    timezone: { type: String, required: true },
+    latitude: { type: Number, required: true },
+    longitude: { type: Number, required: true },
+    language: { type: String, required: true },
+    region: { type: String, required: true },
+    currency: { type: String, required: true },
+    attractions: { type: [String], required: true },
+    image: { type: String, required: true }, 
+  },
+  {
+    timestamps: true,
+  }
+);
+
+let City = model(collection, citySchema);
+
+export default City;
