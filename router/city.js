91376d32232fb71bfdbed076110b76068ad09d36
Alejandro Vargas <alejovastillo@gmail.com>
2024-10-31 17:13:41 -0500

    delete and update city

--- router/city.js
+++ router/city.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { cities, nameCity, idCity } from "../controllers/city/read.js";
+import updateById from "../controllers/city/update.js";
+import deleteCity from "../controllers/city/delete.js";
 
 const router = Router();
 
 router.get("/all", cities);
 router.get("/name/:text", nameCity);
 router.get("/idcity/:id", idCity);
+router.put("/updateCity",updateById)
+router.delete("/deleteCity/:id", deleteCity)
 
 export default router;
