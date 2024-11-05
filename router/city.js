d1928aed1f7ef8166d439150c77fc85c92b8740f
Alejandro Vargas <alejovastillo@gmail.com>
2024-11-05 17:15:40 -0500

    changes

--- router/city.js
+++ router/city.js
@@ -3,11 +3,10 @@ import { cities, nameCity, idCity } from "../controllers/city/read.js";
 import updateByIdCity from "../controllers/city/update.js";
 import deleteCity from "../controllers/city/delete.js";
 import createCity from "../controllers/city/create.js";
-import validateMethod from "../middlewares/validateMethod.js";
 
 const router = Router();
 
-router.get("/all", validateMethod(['GET']), cities);
+router.get("/all", cities);
 router.get("/name/:text", nameCity);
 router.get("/id/:id", idCity);
 router.post('/create', createCity)
